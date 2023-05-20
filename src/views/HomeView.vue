ac1810e0861146ce708047184cec62c2089202b0
ineumann <cloud@inovhub.com>
2023-05-20 11:49:17 +0100

    Update Image

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <img src="../assets/imgs/profile.png"
+        <img src="../assets/imgs/profile.jpeg"
             class="w-[180px] h-[180px] sm:w-[200px] sm:h-[200px] mx-auto rounded-full shadow-md">
         <h1 class="mt-8 mb-4 text-center text-3xl sm:text-4xl  font-bold dark:text-white">Brahem Abdelkader</h1>
         <hr class="border-1 my-4 border-gray-400 w-14 mx-auto ">
